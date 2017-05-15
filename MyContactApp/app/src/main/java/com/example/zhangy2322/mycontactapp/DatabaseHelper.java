0b3f6c72970314782ae4024cba44f22a94301e4f
czhang51 <c.zhang51@gmail.com>
2017-05-15 09:13:56 -0700

    Added entries to database (email, phone) Have not tested

--- MyContactApp/app/src/main/java/com/example/zhangy2322/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/zhangy2322/mycontactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.zhangy2322.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,6 +14,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+    public static final String COL_3 = "PHONE";
+    public static final String COL_4 = "EMAIL";
 
 
 
@@ -29,5 +32,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name, String phone, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, phone);
+        contentValues.put(COL_4, email);
+
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
--- MyContactApp/app/src/main/java/com/example/zhangy2322/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/zhangy2322/mycontactapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.zhangy2322.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,4 +26,25 @@ public class MainActivity extends AppCompatActivity {
         myDb = new DatabaseHelper(this);
 
     }
+
+    public void addData(View v) {
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editPhone.getText().toString(), editEmail.getText().toString());
+
+        if (isInserted == true) {
+            Log.d("MyContact", "Data insertion successful");
+
+            //Create toast message to user indicating data inserted correctly
+            Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Log.d("MyContact", "Data insertion unsuccessful");
+
+            //Create toast message to user indicating data inserted incorrectly
+            Toast.makeText(getApplicationContext(), "Data insertion failed :(", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+
 }
